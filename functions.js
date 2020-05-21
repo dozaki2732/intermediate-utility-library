8bcf80a6f451e8eb9b721702bb7442ed5059c7d0
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-05-21 10:38:55 -0700

    making changes

--- functions.js
+++ functions.js
@@ -1,1 +1,54 @@
-// Your functions go here!
+//counting cards
+function countCards(card) {
+   //var count = 0;
+   switch (card) {
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+         count++;
+         break;
+      case 10:
+      case "J":
+      case "Q":
+      case "K":
+      case "A":
+         count--;
+         break;
+   }
+   if (count > 0) {
+      return "Bet";
+   } else {
+      return "Hold";
+   }
+}
+function lookUpProfile(name, profile) {
+   let contacts = [
+      {
+        "firstName": "Tatsuro",
+        "lastName": "Yamashita",
+        "number": "27323025",
+        "likes": ["flowers", "nature", "animals"]
+      },
+      {
+        "firstName": "",
+        "lastName": "Laine",
+        "number": "12191871",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+      }
+      {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "3171438",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+      }
+      {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "5003299868",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+      }
+   ];
+
+}
