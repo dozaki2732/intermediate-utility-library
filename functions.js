8e91533b808aebf47129b6f62d2ddeab1540a54a
dozaki2732 <64185224+dozaki2732@users.noreply.github.com>
2020-06-09 14:32:02 -0700

    fixed index comments

--- functions.js
+++ functions.js
@@ -54,40 +54,40 @@ var contacts = [
 ];
 
 function lookUpProfile(name, prop) {
+   //create a for loop
    for (var i = 0; i < contacts.length; i++) {
+      // if the name inputed = to the name within the contacts it will return that property specific to that name
       if (contacts[i].firstName === name) {
-         if (prop in contacts[i]) {
-            return contacts[i][prop];
-         } else return "No such property";
+         //if there is no such property, it will return "no such property"
+         return contacts[i][prop] || "no such property";
       }
    }
-   return "no such contact";
+   return "no such name";
 }
 
+//generate a random number between the min and max input
 function generateWholeNumbers(min, max) {
    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+//remove numbers with a splice using starting point (0 base coutning) and quanitity removed
 function removeWithSplice(input1, input2) {
    list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
    list.splice(input1, input2);
    return list;
 }
 
-function removeWithSplice(input1, input2) {
-   list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-   list.splice(input1, input2);
-   return list;
-}
-
+//add with splice
 function addWithSplice(input1, input2, input3) {
    list = [1, 2, 3, 4, 5, 5, 7, 8, 9];
    list.splice(input1, input2, input3);
    return list;
 }
 
+//see if the beer is on the listn using index of
 function checkUsingIndexOf(item) {
    list = ["kirin", "corona", "modelo", "sapporo", "pabst", "stella"];
+   //if the list has item in its index, it will return yes
    if (list.indexOf(item) >= 0) {
       return "yes";
    }
